de160aec6c7f2e04fc7375b9c3be4109e6c72690
LeonardoBnn <boninoleonardo@gmail.com>
2025-11-17 10:20:55 +0100

    liste des pages + routing index.php

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php 
+
+session_start();
+
+require_once("view/commun/header.php");
+$page = isset($_GET['page']) ? $_GET['page'] : 'Accueil';
+
+switch($page){
+
+    case '' :
+        require_once("");
+        break;
+    case '' :
+        require_once("");
+        break;
+    case '' :
+        require_once("");
+        break;
+    case '' :
+        require_once("");
+        break;
+    case '' :
+        require_once("");
+        break;
+    case '' :
+        require_once("");
+        break;
+    case '' :
+        require_once("");
+        break;
+    case '' :
+        require_once("");
+        break;
+    case '' :
+        require_once("");
+        break;
+    case '' :
+        require_once("");
+        break;
+    case '' :
+        require_once("");
+        break;
+    case '' :
+        require_once("");
+        break;
+    case '' :
+        require_once("");
+        break;
+    case '' :
+        require_once("");
+        break;
+    case '' :
+        require_once("");
+        break;
+    case '' :
+        require_once("");
+        break;
+    case '' :
+        require_once("");
+        break;
+    case '' :
+        require_once("");
+        break;
+    case '' :
+        require_once("");
+        break;
+}
+
+require_once("view/commun/footer.php");
+
+?>
