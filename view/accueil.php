aa789e71149d5459c72d4f65cce35bd12c6ee962
Kristof-sZn <siazon.jeanchristophe.pro@gmail.com>
2025-12-02 10:19:21 +0100

    accueil.php

---
+++ view/accueil.php
@@ -0,0 +1,13 @@
+
+
+<section class="main-section">
+    <h2>Bienvenue sur MedInfo</h2>
+    <p>Votre plateforme de prise de rendez-vous médicaux en ligne, simple, rapide et sécurisée.</p>
+    
+    <button class="btn-primary">Prendre rendez-vous</button>
+    
+    <!-- Pastille entreprise -->
+    <div class="backyard-badge">Le Backyard</div>
+</section>
+
+
